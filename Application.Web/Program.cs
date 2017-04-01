b6aeb7cc2e09930d842eefcd2bbc309874702b1a
usausa <machi.pon@gmail.com>
2017-04-01 21:06:55 +0900

    Add hosting.json

--- Application.Web/Program.cs
+++ Application.Web/Program.cs
@@ -1,14 +1,22 @@
 ﻿namespace Application.Web
 {
     using System.IO;
+
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
