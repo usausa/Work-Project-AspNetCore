9ca8570a9e6b371a2e9136b824566b7620b3ee6c
usausa <machi.pon@gmail.com>
2017-04-25 21:28:26 +0900

    Add Smart.Data classes

---
+++ Smart.Data/Data/IDialect.cs
@@ -0,0 +1,11 @@
+﻿namespace Assistance.Infrastructure.Data
+{
+    using System.Data.Common;
+
+    public interface IDialect
+    {
+        bool IsDuplicate(DbException ex);
+
+        string LikeEscape(string value);
+    }
+}
