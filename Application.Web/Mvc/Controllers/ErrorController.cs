7ec404a6ff1602207e8950c94a0cd3c179f6d385
usausa <machi.pon@gmail.com>
2017-05-05 15:27:50 +0900

    Update Controllers

---
+++ Application.Web/Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Web.Mvc.Controllers
+{
+    using Application.Web.Mvc.Infrastructure;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [AllowAnonymous]
+    public class ErrorController : BaseMvcController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+    }
+}
