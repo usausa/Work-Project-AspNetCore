1b692755867aee46c70fb1f4b66db795ef0999d0
usausa <machi.pon@gmail.com>
2018-03-21 17:46:02 +0900

    Update page and view model

--- OouiApplication/OouiApplication/Pages/HomePageViewModel.cs
+++ OouiApplication/OouiApplication/Pages/HomePageViewModel.cs
@@ -1,6 +1,18 @@
 ﻿namespace OouiApplication.Pages
 {
-    public class HomePageViewModel
+    using Smart.ComponentModel;
+    using Smart.Forms.Input;
+    using Smart.Forms.ViewModels;
+
+    public class HomePageViewModel : ViewModelBase
     {
+        public NotificationValue<int> Counter { get; } = new NotificationValue<int>();
+
+        public DelegateCommand IncrementCommand { get; }
+
+        public HomePageViewModel()
+        {
+            IncrementCommand = MakeDelegateCommand(() => Counter.Value++);
+        }
     }
 }
