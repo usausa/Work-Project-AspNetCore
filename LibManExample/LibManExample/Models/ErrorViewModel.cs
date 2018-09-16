98f289eb6cab1413959e22d2ca4567ff8031ac0f
usausa <machi.pon@gmail.com>
2018-09-16 20:41:48 +0900

    Add project base

---
+++ LibManExample/LibManExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibManExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
