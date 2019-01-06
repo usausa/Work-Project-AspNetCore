85e29cdfa530f4e76c1678532f08f50983fadb11
usausa <machi.pon@gmail.com>
2019-01-06 21:18:02 +0900

    Update project base

---
+++ SubAreaExample/SubAreaExample/Infrastructure/LowercaseControllerModelConvention.cs
@@ -0,0 +1,16 @@
+﻿namespace SubAreaExample.Infrastructure
+{
+    using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+    public sealed class LowercaseControllerModelConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            controller.ControllerName = controller.ControllerName.ToLower();
+            foreach (var action in controller.Actions)
+            {
+                action.ActionName = action.ActionName.ToLower();
+            }
+        }
+    }
+}
